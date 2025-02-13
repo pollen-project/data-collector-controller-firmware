f4741c9b477647460bd86a8540c5650a7451489b
Botond Horvath <hello@boti7.com>
2025-02-13 10:55:54 +0100

    Add some comments to the code

--- data_collector/main.cpp
+++ data_collector/main.cpp
@@ -18,14 +18,16 @@
 #include "GPS.h"
 #include "Sensors.h"
 
-#define GPIO_NBIOT_RST 2
-#define GPIO_POWER_GPS 3
-#define GPIO_POWER_SENSORS 20
-#define GPIO_DHT1 13
-#define GPIO_DHT2 15
-#define GPIO_PGOOD 21
-#define GPIO_CHG 22
+// Hardware IO pins
+#define GPIO_NBIOT_RST 2        // NB-IoT module reset
+#define GPIO_POWER_GPS 3        // 5v power on/off
+#define GPIO_POWER_SENSORS 20   // 3.3v power on/off
+#define GPIO_DHT1 13            // Inside sensor data input
+#define GPIO_DHT2 15            // Outside sensor data input
+#define GPIO_PGOOD 21           // Charge controller "power good" signal
+#define GPIO_CHG 22             // Charge controller "is charging" signal
 
+// Hardware UART config for the NB-IoT and GPS modules
 #define UART_NBIOT_ID uart0
 #define UART_NBIOT_BAUD_RATE 115200
 #define UART_NBIOT_TX_PIN 0
@@ -35,27 +37,28 @@
 #define UART_GPS_TX_PIN 8
 #define UART_GPS_RX_PIN 9
 
+// Generic UART config
 #define DATA_BITS 8
 #define STOP_BITS 1
 #define PARITY    UART_PARITY_NONE
 
+// Jems JSON library config
+#define JSON_MAX_LEVEL 10
+
+// Enable/disable parts of the firmware
 #define MODULE_NBIOT_ENABLE true
 #define MODULE_GPS_ENABLE true
 #define MODULE_ENERGY_ENABLE true
 
-#define JSON_MAX_LEVEL 10
-
+// Measurement intervals
 #define POWER_AVG_COUNT 1
-#define POWER_AVG_READING_COUNT 60   // 60
-#define WAKE_INTERVAL_MS 10000   // 10000
+#define POWER_AVG_READING_COUNT 60
+#define WAKE_INTERVAL_MS 10000
 #define WAKE_TIMEOUT_MS 120000
 #define REPORT_INTERVAL_MS (1 * 60 * 1000)
 #define GPS_INTERVAL 8640     // 1h: 360; 24h: 8640
 
 using namespace std;
-
-repeating_timer timer;
-repeating_timer power_timer;
 
 int power_avg_count = 0;
 int power_reading_count = 0;
@@ -73,12 +76,14 @@ static bool awake;
 static volatile bool mqtt_ready = false;
 static volatile bool gps_ready = true;
 
-bool power_timer_callback(__unused struct repeating_timer *t);
+bool do_measurements();
 void sleep();
 void on_nbiot_rx();
 void on_gps_rx();
 void send_gps_data();
 
+// Initialize MQTT, GPS, and Sensors modules
+// MQTT and GPS have callbacks for when they are ready to sleep
 MQTT mqtt(UART_NBIOT_ID, [](bool ready) {
     cout << "mqtt ready: " << ready << endl;
     mqtt_ready = ready;
@@ -89,6 +94,7 @@ GPS gps(UART_GPS_ID, GPIO_POWER_GPS, [] {
 });
 Sensors sensors(GPIO_POWER_SENSORS);
 
+// Handle waking from sleep mode
 static void alarm_sleep_callback(uint alarm_id) {
     printf("alarm woke us up\n");
     uart_default_tx_wait_blocking();
@@ -97,10 +103,11 @@ static void alarm_sleep_callback(uint alarm_id) {
     hardware_alarm_unclaim(alarm_id);
 }
 
+// Wait for all modules to be ready and go to sleep
 void sleep() {
     absolute_time_t sleep_start_time = get_absolute_time();
     cout << "waiting for mqtt & gps... " << sleep_start_time << endl;
-    while (true) {  // !mqtt_ready || !gps_ready
+    while (true) {
         gpio_put(PICO_DEFAULT_LED_PIN, true);
         sleep_ms(100);
         gpio_put(PICO_DEFAULT_LED_PIN, false);
@@ -111,6 +118,7 @@ void sleep() {
             break;
         }
 
+        // Stop waiting after WAKE_TIMEOUT_MS has passed
         absolute_time_t now = get_absolute_time();
 
         if (sleep_start_time + (WAKE_TIMEOUT_MS * 1000) < now) {
@@ -123,12 +131,11 @@ void sleep() {
         tight_loop_contents();
     }
     cout << "sleeping" << endl;
-    // gpio_put(PICO_DEFAULT_LED_PIN, true);
-    // sleep_ms(1000);
-    // gpio_put(PICO_DEFAULT_LED_PIN, false);
 
+    // Turn off GPS
     gps.stop();
 
+    // Disable UART RX interrupts to prevent unwanted wake
     int UART_NBIOT_IRQ = UART_NBIOT_ID == uart0 ? UART0_IRQ : UART1_IRQ;
     irq_set_enabled(UART_NBIOT_IRQ, false);
     irq_clear(UART_NBIOT_IRQ);
@@ -139,9 +146,11 @@ void sleep() {
     irq_clear(UART_GPS_IRQ);
     uart_set_irq_enables(UART_GPS_ID, false, false);
 
+    // Run from external oscillator
     sleep_run_from_xosc();
     awake = false;
 
+    // Put CPU to sleep
     uart_default_tx_wait_blocking();
     if (sleep_goto_sleep_for(WAKE_INTERVAL_MS, &alarm_sleep_callback)) {
         while (!awake) {
@@ -149,8 +158,10 @@ void sleep() {
         }
     }
 
+    // Restore clocks after wake
     sleep_power_up();
 
+    // Enable UART RX interrupts
     UART_NBIOT_IRQ = UART_NBIOT_ID == uart0 ? UART0_IRQ : UART1_IRQ;
     irq_set_exclusive_handler(UART_NBIOT_IRQ, on_nbiot_rx);
     irq_set_enabled(UART_NBIOT_IRQ, true);
@@ -167,30 +178,37 @@ void sleep() {
         gps.get_position_once(send_gps_data);
     }
 
-    power_timer_callback(NULL);
+    do_measurements();
 
+    // Start next sleep cycle
     sleep();
 }
 
+// UART RX handler
 void on_nbiot_rx() {
     mqtt.on_rx();
 }
 
+// UART RX handler
 void on_gps_rx() {
     gps.on_rx();
 }
 
+// JEMS JSON helper
 static void write_char(char ch, uintptr_t arg) {
     *reinterpret_cast<string *>(arg) += ch;
 }
 
 void send_data() {
+    // Read power values
     uint8_t charger_chg = !gpio_get(GPIO_CHG);
     uint8_t charger_pgood = !gpio_get(GPIO_PGOOD);
     power_t *pavg = &power_avg[0];
 
+    // Read sensors
     sensors.read_environment();
 
+    // Construct a JSON object
     json_sensors.clear();
     jems_init(&jems, jems_levels, JSON_MAX_LEVEL, write_char, reinterpret_cast<uintptr_t>(&json_sensors));
 
@@ -217,16 +235,12 @@ void send_data() {
     jems_string(&jems, "power");   //   "power"
     jems_object_open(&jems);             //     {
     jems_string(&jems, "Vsol");
-    // jems_integer(&jems, static_cast<int>(sensors.sensor_data.power.solar.voltage));
     jems_integer(&jems, static_cast<int>(pavg->solar.voltage));
     jems_string(&jems, "Isol");
-    // jems_integer(&jems, static_cast<int>(sensors.sensor_data.power.solar.current));
     jems_integer(&jems, static_cast<int>(pavg->solar.current));
     jems_string(&jems, "Vbat");
-    // jems_integer(&jems, static_cast<int>(sensors.sensor_data.power.battery.voltage));
     jems_integer(&jems, static_cast<int>(pavg->battery.voltage));
     jems_string(&jems, "Ibat");
-    // jems_integer(&jems, static_cast<int>(sensors.sensor_data.power.battery.current));
     jems_integer(&jems, static_cast<int>(pavg->battery.current));
     jems_string(&jems, "is_charging");
     jems_bool(&jems, charger_chg);
@@ -236,6 +250,7 @@ void send_data() {
 
     jems_object_close(&jems);     // }
 
+    // Send JSON object via MQTT
     mqtt.publish(json_sensors.data());
 }
 
@@ -247,6 +262,7 @@ void send_gps_data() {
 
     mqtt_ready = false;
 
+    // Construct a JSON object
     json_gps.clear();
     jems_init(&jems, jems_levels, JSON_MAX_LEVEL, write_char, reinterpret_cast<uintptr_t>(&json_gps));
 
@@ -257,62 +273,15 @@ void send_gps_data() {
 
     jems_object_close(&jems);          // }
 
-    // json_gps = R"({"gps":""})";
+    // Send JSON via MQTT
     mqtt.publish(json_gps.data());
 
     gps.gps_data_ready = false;
     gps_ready = true;
 }
 
-// void send_power_data() {
-//     json_sensors.clear();
-//     jems_init(&jems, jems_levels, JSON_MAX_LEVEL, write_char, reinterpret_cast<uintptr_t>(&json_sensors));
-//
-//     jems_object_open(&jems);            // {
-//     jems_string(&jems, "power_history");   //   "dht22"
-//     jems_array_open(&jems);             //     [
-//
-//     for (int i = 0; i < power_avg_count; i++) {
-//         power_t *pavg = &power_avg[i];
-//
-//         jems_object_open(&jems);             //     {
-//         jems_string(&jems, "timestamp");
-//         jems_string(&jems, pavg->timestamp.c_str());
-//         jems_string(&jems, "Vsol");
-//         jems_integer(&jems, static_cast<int>(pavg->solar.voltage));
-//         jems_string(&jems, "Isol");
-//         jems_integer(&jems, static_cast<int>(pavg->solar.current));
-//         jems_string(&jems, "Vbat");
-//         jems_integer(&jems, static_cast<int>(pavg->battery.voltage));
-//         jems_string(&jems, "Ibat");
-//         jems_integer(&jems, static_cast<int>(pavg->battery.current));
-//         jems_object_close(&jems);
-//     }
-//
-//     jems_array_close(&jems);            //      ],
-//
-//     jems_object_close(&jems);     // }
-//
-//     mqtt.publish(json_sensors.data());
-// }
-
-// bool repeating_timer_callback(__unused struct repeating_timer *t) {
-//     sensors.read_power();
-//     sensors.read_environment();
-//
-//     if (!--gps_interval) {
-//         gps_interval = GPS_INTERVAL;
-//         gps.get_position_once(send_gps_data);
-//     }
-//
-//     send_data();
-//
-//     return true;
-// }
-
-bool power_timer_callback(__unused struct repeating_timer *t) {
+bool do_measurements() {
     sensors.read_power();
-    //sensors.read_environment();
 
     power_t *pavg = &power_avg[power_avg_count];
 
@@ -360,25 +329,31 @@ bool power_timer_callback(__unused struct repeating_timer *t) {
 }
 
 int main() {
+    // Initialize microcontroller hardware
     stdio_init_all();
     rtc_init();
 
+    // Status LED pin as output and turn it on
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     gpio_put(PICO_DEFAULT_LED_PIN, true);
 
+    // 5v power control pin as output and turn it off
     gpio_init(GPIO_POWER_GPS);
     gpio_set_dir(GPIO_POWER_GPS, GPIO_OUT);
     gpio_put(GPIO_POWER_GPS, false);
 
+    // 3.3v power control pin as output and turn it off
     gpio_init(GPIO_POWER_SENSORS);
     gpio_set_dir(GPIO_POWER_SENSORS, GPIO_OUT);
     gpio_put(GPIO_POWER_SENSORS, false);
 
+    // Charger "power good" signal pin as input with pull-up
     gpio_init(GPIO_PGOOD);
     gpio_set_dir(GPIO_PGOOD, GPIO_IN);
     gpio_pull_up(GPIO_PGOOD);
 
+    // Charger "is charging" signal pin as input with pull-up
     gpio_init(GPIO_CHG);
     gpio_set_dir(GPIO_CHG, GPIO_IN);
     gpio_pull_up(GPIO_CHG);
@@ -389,31 +364,20 @@ int main() {
     gpio_put(PICO_DEFAULT_LED_PIN, false);
 
 #if MODULE_NBIOT_ENABLE
+    // Set NB-IoT module reset pin as output
     gpio_init(GPIO_NBIOT_RST);
     gpio_set_dir(GPIO_NBIOT_RST, GPIO_OUT);
     gpio_put(GPIO_NBIOT_RST, true);
 
+    // Initialize UART for NB-IoT
     uart_init(UART_NBIOT_ID, UART_NBIOT_BAUD_RATE);
     gpio_set_function(UART_NBIOT_TX_PIN, UART_FUNCSEL_NUM(UART_NBIOT_ID, UART_TX_PIN));
     gpio_set_function(UART_NBIOT_RX_PIN, UART_FUNCSEL_NUM(UART_NBIOT_ID, UART_RX_PIN));
     uart_set_hw_flow(UART_NBIOT_ID, false, false);
     uart_set_format(UART_NBIOT_ID, DATA_BITS, STOP_BITS, PARITY);
     uart_set_fifo_enabled(UART_NBIOT_ID, false);
-#endif
 
-#if MODULE_GPS_ENABLE
-    uart_init(UART_GPS_ID, UART_GPS_BAUD_RATE);
-    gpio_set_function(UART_GPS_TX_PIN, UART_FUNCSEL_NUM(UART_GPS_ID, UART_TX_PIN));
-    gpio_set_function(UART_GPS_RX_PIN, UART_FUNCSEL_NUM(UART_GPS_ID, UART_RX_PIN));
-    uart_set_hw_flow(UART_GPS_ID, false, false);
-    uart_set_format(UART_GPS_ID, DATA_BITS, STOP_BITS, PARITY);
-    uart_set_fifo_enabled(UART_GPS_ID, false);
-#endif
-
-    // Set up a RX interrupt
-    // We need to set up the handler first
-    // Select correct interrupt for the UART we are using
-#if MODULE_NBIOT_ENABLE
+    // UART RX interrupt handler
     int UART_NBIOT_IRQ = UART_NBIOT_ID == uart0 ? UART0_IRQ : UART1_IRQ;
     irq_set_exclusive_handler(UART_NBIOT_IRQ, on_nbiot_rx);
     irq_set_enabled(UART_NBIOT_IRQ, true);
@@ -421,6 +385,15 @@ int main() {
 #endif
 
 #if MODULE_GPS_ENABLE
+    // Initialize UART for GPS
+    uart_init(UART_GPS_ID, UART_GPS_BAUD_RATE);
+    gpio_set_function(UART_GPS_TX_PIN, UART_FUNCSEL_NUM(UART_GPS_ID, UART_TX_PIN));
+    gpio_set_function(UART_GPS_RX_PIN, UART_FUNCSEL_NUM(UART_GPS_ID, UART_RX_PIN));
+    uart_set_hw_flow(UART_GPS_ID, false, false);
+    uart_set_format(UART_GPS_ID, DATA_BITS, STOP_BITS, PARITY);
+    uart_set_fifo_enabled(UART_GPS_ID, false);
+
+    // UART RX interrupt handler
     int UART_GPS_IRQ = UART_GPS_ID == uart0 ? UART0_IRQ : UART1_IRQ;
     irq_set_exclusive_handler(UART_GPS_IRQ, on_gps_rx);
     irq_set_enabled(UART_GPS_IRQ, true);
@@ -433,19 +406,19 @@ int main() {
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
 
     sensors.init();
-
-    // add_repeating_timer_ms(REPORT_INTERVAL_MS, repeating_timer_callback, nullptr, &timer);
-    // add_repeating_timer_ms(POWER_INTERVAL_MS, power_timer_callback, nullptr, &power_timer);
 #endif
 
+    // The NB-IoT module needs a reset signal after power on
     std::cout << "Resetting NB-IoT board... ";
     gpio_put(GPIO_NBIOT_RST, false);
     sleep_ms(500);
     gpio_put(GPIO_NBIOT_RST, true);
     std::cout << "done" << std::endl;
 
+    // Start the main sleep-wake cycle
     sleep();
 
+    // Infinite loop to keep the main function busy
     while (1)
         tight_loop_contents();
 
